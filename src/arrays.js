f0b157fe85fa9e403407402c8996953d6caa37b5
unknown <arian.angoma@tecsup.edu.pe>
2021-07-16 04:43:53 -0500

    Arrays Jest

---
+++ src/arrays.js
@@ -0,0 +1,21 @@
+const carsList = () => {
+    return [
+        'VW Golf GTI',
+        'Honda Civic Type R',
+        'Audi A3',
+        'SEAT Leon'
+    ]
+}
+
+const brandsList = () => {
+    return [
+        'Volkswagen',
+        'Honda',
+        'Audi',
+        'SEAT'
+    ]
+}
+
+module.exports = {
+    carsList
+}
---
+++ test/arrays.test.js
@@ -0,0 +1,15 @@
+const {carsList} = require('../src/arrays');
+
+describe('Verificando autos existentes', () => {
+    test('Que el arreglo no sea null', () => {
+        expect(carsList()).not.toBeNull();
+    });
+
+    test('Verificar si contiene un VW Golf', () => {
+        expect(carsList()).toContain('VW Golf GTI');
+    });
+
+    test('Comprobar la longitud del arreglo', () => {
+        expect(carsList()).toHaveLength(4);
+    });
+})
